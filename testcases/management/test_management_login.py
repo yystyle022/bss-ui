2188fa3f09686569e0046105298b3f99e763c5ac
yangyang <1587882751@qq.com>
2023-04-26 21:10:56 +0800

    添加管理端登录方法

---
+++ testcases/management/test_management_login.py
@@ -0,0 +1,23 @@
+# -*- coding=utf-8 -*-
+# @Time : 2023/4/26 10:40
+# @Author : yangyang
+# @File : bss-ui/test_management_login.py
+
+import allure
+from page.LoginPage import LoginPage
+
+
+@allure.feature('管理端登录验证')
+class TestManagementLogin():
+
+
+    @allure.story('登录管理端')
+    @allure.description('登录管理端')
+    def test_login_management(self, driver, managementUrl, managementUser, managementPwd):
+        '''
+        管理端登录
+        @param driver:
+        @return:
+        '''
+        with allure.step('登录'):
+            LoginPage().login_management(driver, managementUrl, managementUser, managementPwd)
