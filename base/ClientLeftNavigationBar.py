0c2d434f5c1e3226cf9d58dc61bf6269d04cbb26
yangyang <1587882751@qq.com>
2023-05-01 21:27:21 +0800

    增加客户端导航栏Xpath定位

---
+++ base/ClientLeftNavigationBar.py
@@ -0,0 +1,27 @@
+class ClientLeftNavigationBar():
+    def consoleOverviewXpath(self):
+        '''
+        控制台概览
+        @return:
+        '''
+        return "//span[text()='控制台概览']"
+
+    def accountManagementXpath(self):
+        '''
+        服务号管理Xpath
+        @return:
+        '''
+        return "//span[text()='服务号管理']"
+
+    def accountCmXpath(self):
+        '''
+        厘米级服务
+        @return:
+        '''
+        return "//span[text()='厘米级服务']"
+
+    def accountCmOverviewXpath(self):
+        '''
+        厘米级服务概览
+        @return:
+        '''
